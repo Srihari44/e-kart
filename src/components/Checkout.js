bfc3877b33fd5124d635191fd95d7feb6a5a6321
srihari44 <srhrsrihari@gmail.com>
2021-03-11 15:02:19 +0530

    update cart style, remove navbar toggle

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,6 +2,8 @@ import React, { useContext, useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { ProductsContext } from "../providers/StoreProvider";
 import { Button, ListGroup } from "react-bootstrap";
+import FindColor from "./FindColor";
+
 function Checkout(props) {
   const [state, dispatch] = useContext(ProductsContext);
   const [total, totalHandler] = useState(0);
@@ -34,31 +36,65 @@ function Checkout(props) {
     <React.Fragment>
       <div className="">
         {state.checkedOutProducts.length ? (
-          <div className="mt-3 p-3 bg-white">
-            <h5 className="text-center">Cart Items</h5>
-            <ListGroup>
+          <div className="mt-5 p-3 bg-white d-flex flex-column justify-content-center align-items-center flex-wrap">
+            <p className="text-center" style={{ fontSize: "2.5rem" }}>
+              Cart Items
+            </p>
+            <ListGroup style={{ minWidth: "45%" }}>
               {state.checkedOutProducts.map((item) => (
                 <ListGroup.Item
-                  style={{ height: "200px" }}
-                  className="d-flex mx-lg-5 align-items-center justify-content-between"
+                  style={{ height: "auto" }}
+                  className="d-flex align-items-center justify-content-center"
                   key={item.id}
                 >
-                  <img
-                    alt={item.title}
-                    className="h-100"
-                    src={item.image}
-                  ></img>
-                  {item.title} Quantity:{item.count} Price:{item.price}{" "}
-                  Subtotal:{item.subtotal}
-                  <Button onClick={() => updateProductCount(item.id)}>+</Button>
-                  <Button onClick={() => updateProductCount(item.id, true)}>
-                    -
-                  </Button>
-                  <Button onClick={() => removeProduct(item.id)}>Remove</Button>
+                  <div className="d-flex flex-column h-100 mr-lg-5">
+                    <img
+                      style={{ height: "150px", width: "125px" }}
+                      alt={item.title}
+                      src={item.image}
+                    />
+                    <div className="d-flex justify-content-between align-items-center pt-3">
+                      <Button
+                        disabled={item.count <= 1}
+                        onClick={() => updateProductCount(item.id, true)}
+                      >
+                        -
+                      </Button>
+                      {item.count}
+                      <Button
+                        disabled={item.count >= 10}
+                        onClick={() => updateProductCount(item.id)}
+                      >
+                        +
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="ml-5">
+                    <p style={{ fontSize: "1.25rem" }}>{item.title}</p>
+                    <p
+                      style={{
+                        width: "fit-content",
+                        backgroundColor: FindColor(item.category)[0],
+                        color: FindColor(item.category)[1],
+                      }}
+                      className="mb-2 rounded p-1 category"
+                    >
+                      # {item.category}
+                    </p>
+                    <p
+                      className="font-weight-bold"
+                      style={{ fontSize: "1.25rem" }}
+                    >
+                      ${item.subtotal}
+                    </p>
+                    <Button onClick={() => removeProduct(item.id)}>
+                      Remove
+                    </Button>
+                  </div>
                 </ListGroup.Item>
               ))}
             </ListGroup>
-            <h3 className="text-primary text-center mt-3">Total: {total}</h3>
+            <h3 className="text-primary text-center mt-3">Total: ${total}</h3>
           </div>
         ) : (
           <div className="text-center py-3 text-white d-flex flex-column align-items-center">
@@ -66,12 +102,15 @@ function Checkout(props) {
               Your Cart is empty
             </p>
             <img
-                              alt="Dustbin"
-                              className="d-block"
-                              style={{height:"300px" }}
+              alt="Empty Dustbin"
+              className="d-block"
+              style={{ height: "300px" }}
               src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP._oprlqm7BgiqEBezu4N3TQHaHa%26pid%3DApi&f=1"
             ></img>
-            <Button className="my-3 mt-5" onClick={() => props.history.push("/")}>
+            <Button
+              className="my-3 mt-5"
+              onClick={() => props.history.push("/")}
+            >
               Return to Home
             </Button>
           </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,44 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink, withRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { ProductsContext } from "../providers/StoreProvider";
 import React, { useContext } from "react";
 import { IconButton, Badge } from "@material-ui/core";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 
-function NavBar(props) {
+function NavBar() {
   const [state] = useContext(ProductsContext);
 
-  const redirectHandler = () => {
-    props.history.push("/checkout");
-  };
-
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" sticky="top">
       <NavLink className="navbar-brand" to="/">
         E-Kart
       </NavLink>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto"></Nav>
-          <IconButton onClick={redirectHandler}>
-            <Badge
-              badgeContent={state.checkedOutProducts.length}
-              color="secondary"
-            >
-              <ShoppingCartOutlinedIcon style={{ color: "#eee" }} />
-            </Badge>
-          </IconButton>
-          <Nav>
-            {!state.user ? (
-              <NavLink className="nav-link" to="/login">
-                Login
-              </NavLink>
-            ) : (
-              <NavLink className="nav-link" to="/inventry">
-                Inventry
-              </NavLink>
-            )}
-          </Nav>
+        <Nav className="px-lg-4">
+          <NavLink to="/checkout">
+            <IconButton className="nav-link py-2 pt-1 mr-md-4">
+              <Badge
+                badgeContent={state.checkedOutProducts.length}
+                color="secondary"
+              >
+                <ShoppingCartOutlinedIcon />
+              </Badge>
+            </IconButton>
+          </NavLink>
+          {!state.user ? (
+            <NavLink className="nav-link" to="/login">
+              Login
+            </NavLink>
+          ) : (
+            <NavLink className="nav-link" to="/inventry">
+              Inventry
+            </NavLink>
+          )}
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 }
-export default withRouter(NavBar);
+export default NavBar;
